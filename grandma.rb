3cd67d5ba87eaec21e4d2c5451c676d75c0d83e9
decent-scraper-1450 <jaimealba1574@gmail.com>
2020-04-14 20:08:34 +0000

    Automatically backed up by Learn

--- grandma.rb
+++ grandma.rb
@@ -15,8 +15,8 @@ def speak_to_grandma(greeting)
   if greeting.upcase == 'I LOVE YOU GRANDMA!'
     'I LOVE YOU TOO PUMPKIN!'
 elsif greeting.upcase == greeting
-    return "NO, NOT SINCE 1938!"
+    return "HUH?! SPEAK UP, SONNY!"
   else
-    return
+    return "NO, NOT SINCE 1938!"
   end
 end
